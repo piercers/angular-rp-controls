f484253eedacf273e107f2f3ed642f6380edf03f
Ryan Pierce <ry@nspierce.com>
2016-11-14 14:13:56 -0700

    Fixes for RpFormArrayAddDirective.

--- src/rp-form-array-add.directive.ts
+++ src/rp-form-array-add.directive.ts
@@ -41,25 +41,14 @@ const configToGroup = flow(
   config => new FormGroup(config) // Config -> FormGroup
 );
 
-// TODO Currenly, this directive is only barely useful
-// - Maybe it handles array label too?
-//   - This is something that would be often styled the same for this use case
 // - Could also handle initialization of the array
 //   - But should it?
 //   - Does this control start with one empty control if none exisiting?
 //   - If all are removed, is an empty one inserted?
-// TODO Naming: rpFormArray...
-// - Add, Section, Header, Title, Label
-// TODO Should it add styling to rp-form-array-label and functionality to [rpFormArrayAdd]?
-// - Can't decide if this is needed or helpful
-//   - If you just need the functionality, write a method to that component?
-// - Could replace having to do `<div formArrayName="arrayControl">`
-//   - I think this is needed in order to do `<rp-text-control [formControlName]="i"></rp-text-control>`
-//   - `<rp-form-array label="Array Control" [formArray]="arrayControl" [intialize]="true"></rp-form-array>`
-// - Have the directive to the heavylifting, then a component that extends this.
+// - `<rp-form-array label="Array Control" [formArray]="arrayControl" [intialize]="true"></rp-form-array>`
 @Directive({selector: '[rpFormArrayAdd]'})
 export class RpFormArrayAddDirective implements OnInit {
-  @Input() rpFormArrayLabel: FormArray;
+  @Input() rpFormArrayAdd: FormArray;
 
   private array: FormArray;
 
@@ -68,7 +57,7 @@ export class RpFormArrayAddDirective implements OnInit {
   constructor(@Optional() private formArray: FormArrayName) {}
 
   ngOnInit() {
-    this.array = this.rpFormArrayLabel || this.formArray.control; // TODO What if a parent formArray isn't given? Error?
+    this.array = this.rpFormArrayAdd || this.formArray.control; // TODO What if a parent formArray isn't given? Error?
 
     const first = firstControl(this.array);
 
