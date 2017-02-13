df69ec951360a4b2967f1563835ede52c130d855
Ryan Pierce <ryanpierce@Supports-MacBook-Pro.local>
2017-02-13 15:45:23 -0700

    Better dropshadow.

--- src/ui/rp-controls-dropdown.component.ts
+++ src/ui/rp-controls-dropdown.component.ts
@@ -10,7 +10,11 @@ import {RpControlsSettingsService} from '../rp-controls-settings.service';
       display: none;
       left: 0;
       z-index: 1000;
-      box-shadow: 0 0 1px rgba(0,0,0,0.15);
+      box-shadow:
+        0 5px 5px -3px rgba(0,0,0,.2),
+        0 8px 10px 1px rgba(0,0,0,.14),
+        0 3px 14px 2px rgba(0,0,0,.12)
+      ;
       background-color: white;
     }
 
